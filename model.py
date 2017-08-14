e6d4572eef0f343bff58349fce26bf21bf4f6167
Amr Mohamed Nasr <re13amr@gmail.com>
2017-08-14 17:30:16 +0200

    model fix

--- model.py
+++ model.py
@@ -249,9 +249,11 @@ class Model():
 			self.momentum = tf.Variable(0.0, trainable=False)
 			tvars = tf.trainable_variables()
 
-		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d/cpu:0" % self.task_index,cluster=cluster)):
+		with tf.device("/job:worker/task:0/cpu:0"):
+			logger.write("Task 0 worker")
 			testGradient2 = tf.gradients(self.cost, tvars[len(tvars)/2:])
-		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d/gpu:0" % self.task_index,cluster=cluster)):
+		with tf.device("/job:worker/task:1/gpu:0"):
+			logger.write("Task 1 worker")
 			testGradient1 = tf.gradients(self.cost, tvars[:len(tvars)/2])
 
 			testGradient = testGradient1+testGradient2
