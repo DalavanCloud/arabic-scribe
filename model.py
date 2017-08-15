07242dc78d8bd629d4654eedd2c3c8820b33bfb6
Amr Mohamed Nasr <re13amr@gmail.com>
2017-08-15 13:40:48 +0200

    model fix in previous commit

--- model.py
+++ model.py
@@ -49,6 +49,97 @@ class Model():
 
 
 		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d" % self.task_index,cluster=cluster)):
+			# ----- build the gaussian character window
+			def get_window(alpha, beta, kappa, c):
+				# phi -> [? x 1 x ascii_steps] and is a tf matrix
+				# c -> [? x ascii_steps x alphabet] and is a tf matrix
+				ascii_steps = c.get_shape()[1].value #number of items in sequence
+				phi = get_phi(ascii_steps, alpha, beta, kappa)
+				# Window shape ==> 32,1,54
+				window = tf.matmul(phi,c)
+				# Window shape ==> 32,54
+				window = tf.squeeze(window, [1]) # window ~ [?,alphabet]
+				return window, phi
+
+			#get phi for all t,u (returns a [1 x tsteps] matrix) that defines the window
+			def get_phi(ascii_steps, alpha, beta, kappa):
+				# alpha, beta, kappa -> [?,kmixtures,1] and each is a tf variable
+				# np.linspace ==> Creates a float array starting from 0 till ascii_steps -1
+				u = np.linspace(0,ascii_steps-1,ascii_steps) # weight all the U items in the sequence
+				# Kappa_term will have 32 columns, 1 row, each row contains 6
+				# Subtraction and multiplication supports broadcasting CHECK : https://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
+				kappa_term = tf.square( tf.subtract(kappa,u))
+				# exp_term shape ==> 32,1,6
+				# beta shape ==> 32,1,1
+				exp_term = tf.multiply(-beta,kappa_term)
+				# alpha shape ==> 32,1,1
+				# phi_k shape ==> 32,1,6
+				phi_k = tf.multiply(alpha, tf.exp(exp_term))
+				# phi shape ==> 32,1,6
+				# reduce sum keeps the dimensions but sums up the elements in each dimension
+				# Why?
+				phi = tf.reduce_sum(phi_k,1, keep_dims=True)
+				return phi # phi ~ [?,1,ascii_steps]
+
+			def get_window_params(i, out_cell0, kmixtures, prev_kappa, reuse=True):
+				hidden = out_cell0.get_shape()[1]
+				n_out = 3*kmixtures
+				with tf.variable_scope('window',reuse=reuse):
+					window_w = tf.get_variable("window_w", [hidden, n_out], initializer=self.graves_initializer)
+					window_b = tf.get_variable("window_b", [n_out], initializer=self.window_b_initializer)
+
+				# Matrix multiplication of X (2D tensor in this case, the output of cell 0), with the Weight, and adds the Bias
+				# w.x + b 
+				abk_hats = tf.nn.xw_plus_b(out_cell0, window_w, window_b) # abk_hats ~ [?,n_out]
+				# e^ tf reshaped
+				# Why?
+				abk = tf.exp(tf.reshape(abk_hats, [-1, 3*kmixtures,1])) # abk_hats ~ [?,n_out] = "alpha, beta, kappa hats"
+
+				alpha, beta, kappa = tf.split(abk, 3, 1) # alpha_hat, etc ~ [?,kmixtures]
+				kappa = kappa + prev_kappa
+				return alpha, beta, kappa # each ~ [?,kmixtures,1]
+				
+				# ----- build mixture density cap on top of second recurrent cell
+			def gaussian2d(x1, x2, mu1, mu2, s1, s2, rho):
+				# define gaussian mdn (eq 24, 25 from http://arxiv.org/abs/1308.0850)
+				x_mu1 = tf.subtract(x1, mu1)
+				x_mu2 = tf.subtract(x2, mu2)
+				Z = tf.square(tf.div(x_mu1, s1)) + \
+				    tf.square(tf.div(x_mu2, s2)) - \
+				    2*tf.div(tf.multiply(rho, tf.multiply(x_mu1, x_mu2)), tf.multiply(s1, s2))
+				rho_square_term = 1-tf.square(rho)
+				power_e = tf.exp(tf.div(-Z,2*rho_square_term))
+				regularize_term = 2*np.pi*tf.multiply(tf.multiply(s1, s2), tf.sqrt(rho_square_term))
+				gaussian = tf.div(power_e, regularize_term)
+				return gaussian
+
+			def get_loss(pi, x1_data, x2_data, eos_data, mu1, mu2, sigma1, sigma2, rho, eos):
+				# define loss function (eq 26 of http://arxiv.org/abs/1308.0850)
+				gaussian = gaussian2d(x1_data, x2_data, mu1, mu2, sigma1, sigma2, rho)
+				term1 = tf.multiply(gaussian, pi)
+				term1 = tf.reduce_sum(term1, 1, keep_dims=True) #do inner summation
+				term1 = -tf.log(tf.maximum(term1, 1e-20)) # some errors are zero -> numerical errors.
+
+				term2 = tf.multiply(eos, eos_data) + tf.multiply(1-eos, 1-eos_data) #modified Bernoulli -> eos probability
+				term2 = -tf.log(term2) #negative log error gives loss
+
+				return tf.reduce_sum(term1 + term2) #do outer summation
+
+			# now transform dense NN outputs into params for MDN
+			def get_mdn_coef(Z):
+				# returns the tf slices containing mdn dist params (eq 18...23 of http://arxiv.org/abs/1308.0850)
+				eos_hat = Z[:, 0:1] #end of sentence tokens
+				pi_hat, mu1_hat, mu2_hat, sigma1_hat, sigma2_hat, rho_hat = tf.split(Z[:, 1:], 6, 1)
+				self.pi_hat, self.sigma1_hat, self.sigma2_hat = \
+											pi_hat, sigma1_hat, sigma2_hat # these are useful for bias method during sampling
+
+				eos = tf.sigmoid(-1*eos_hat) # technically we gained a negative sign
+				pi = tf.nn.softmax(pi_hat) # softmax z_pi:
+				mu1 = mu1_hat; mu2 = mu2_hat # leave mu1, mu2 as they are
+				sigma1 = tf.exp(sigma1_hat); sigma2 = tf.exp(sigma2_hat) # exp for sigmas
+				rho = tf.tanh(rho_hat) # tanh for rho (squish between -1 and 1)rrr
+
+				return [eos, pi, mu1, mu2, sigma1, sigma2, rho]
 			# ----- build the basic recurrent network architecture
 			# Creates 3 cells, each containing rnn_size (Default = 100) hidden unit (Basically 100 neuron OR sigmoid)
 
@@ -182,97 +273,7 @@ class Model():
 		config = tf.ConfigProto(allow_soft_placement = True)
 		self.sess = sv.prepare_or_wait_for_session(server.target,config=config)
 
-		# ----- build the gaussian character window
-		def get_window(alpha, beta, kappa, c):
-			# phi -> [? x 1 x ascii_steps] and is a tf matrix
-			# c -> [? x ascii_steps x alphabet] and is a tf matrix
-			ascii_steps = c.get_shape()[1].value #number of items in sequence
-			phi = get_phi(ascii_steps, alpha, beta, kappa)
-			# Window shape ==> 32,1,54
-			window = tf.matmul(phi,c)
-			# Window shape ==> 32,54
-			window = tf.squeeze(window, [1]) # window ~ [?,alphabet]
-			return window, phi
 
-		#get phi for all t,u (returns a [1 x tsteps] matrix) that defines the window
-		def get_phi(ascii_steps, alpha, beta, kappa):
-			# alpha, beta, kappa -> [?,kmixtures,1] and each is a tf variable
-			# np.linspace ==> Creates a float array starting from 0 till ascii_steps -1
-			u = np.linspace(0,ascii_steps-1,ascii_steps) # weight all the U items in the sequence
-			# Kappa_term will have 32 columns, 1 row, each row contains 6
-			# Subtraction and multiplication supports broadcasting CHECK : https://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
-			kappa_term = tf.square( tf.subtract(kappa,u))
-			# exp_term shape ==> 32,1,6
-			# beta shape ==> 32,1,1
-			exp_term = tf.multiply(-beta,kappa_term)
-			# alpha shape ==> 32,1,1
-			# phi_k shape ==> 32,1,6
-			phi_k = tf.multiply(alpha, tf.exp(exp_term))
-			# phi shape ==> 32,1,6
-			# reduce sum keeps the dimensions but sums up the elements in each dimension
-			# Why?
-			phi = tf.reduce_sum(phi_k,1, keep_dims=True)
-			return phi # phi ~ [?,1,ascii_steps]
-
-		def get_window_params(i, out_cell0, kmixtures, prev_kappa, reuse=True):
-			hidden = out_cell0.get_shape()[1]
-			n_out = 3*kmixtures
-			with tf.variable_scope('window',reuse=reuse):
-				window_w = tf.get_variable("window_w", [hidden, n_out], initializer=self.graves_initializer)
-				window_b = tf.get_variable("window_b", [n_out], initializer=self.window_b_initializer)
-
-			# Matrix multiplication of X (2D tensor in this case, the output of cell 0), with the Weight, and adds the Bias
-			# w.x + b 
-			abk_hats = tf.nn.xw_plus_b(out_cell0, window_w, window_b) # abk_hats ~ [?,n_out]
-			# e^ tf reshaped
-			# Why?
-			abk = tf.exp(tf.reshape(abk_hats, [-1, 3*kmixtures,1])) # abk_hats ~ [?,n_out] = "alpha, beta, kappa hats"
-
-			alpha, beta, kappa = tf.split(abk, 3, 1) # alpha_hat, etc ~ [?,kmixtures]
-			kappa = kappa + prev_kappa
-			return alpha, beta, kappa # each ~ [?,kmixtures,1]
-			
-			# ----- build mixture density cap on top of second recurrent cell
-		def gaussian2d(x1, x2, mu1, mu2, s1, s2, rho):
-			# define gaussian mdn (eq 24, 25 from http://arxiv.org/abs/1308.0850)
-			x_mu1 = tf.subtract(x1, mu1)
-			x_mu2 = tf.subtract(x2, mu2)
-			Z = tf.square(tf.div(x_mu1, s1)) + \
-			    tf.square(tf.div(x_mu2, s2)) - \
-			    2*tf.div(tf.multiply(rho, tf.multiply(x_mu1, x_mu2)), tf.multiply(s1, s2))
-			rho_square_term = 1-tf.square(rho)
-			power_e = tf.exp(tf.div(-Z,2*rho_square_term))
-			regularize_term = 2*np.pi*tf.multiply(tf.multiply(s1, s2), tf.sqrt(rho_square_term))
-			gaussian = tf.div(power_e, regularize_term)
-			return gaussian
-
-		def get_loss(pi, x1_data, x2_data, eos_data, mu1, mu2, sigma1, sigma2, rho, eos):
-			# define loss function (eq 26 of http://arxiv.org/abs/1308.0850)
-			gaussian = gaussian2d(x1_data, x2_data, mu1, mu2, sigma1, sigma2, rho)
-			term1 = tf.multiply(gaussian, pi)
-			term1 = tf.reduce_sum(term1, 1, keep_dims=True) #do inner summation
-			term1 = -tf.log(tf.maximum(term1, 1e-20)) # some errors are zero -> numerical errors.
-
-			term2 = tf.multiply(eos, eos_data) + tf.multiply(1-eos, 1-eos_data) #modified Bernoulli -> eos probability
-			term2 = -tf.log(term2) #negative log error gives loss
-
-			return tf.reduce_sum(term1 + term2) #do outer summation
-
-		# now transform dense NN outputs into params for MDN
-		def get_mdn_coef(Z):
-			# returns the tf slices containing mdn dist params (eq 18...23 of http://arxiv.org/abs/1308.0850)
-			eos_hat = Z[:, 0:1] #end of sentence tokens
-			pi_hat, mu1_hat, mu2_hat, sigma1_hat, sigma2_hat, rho_hat = tf.split(Z[:, 1:], 6, 1)
-			self.pi_hat, self.sigma1_hat, self.sigma2_hat = \
-										pi_hat, sigma1_hat, sigma2_hat # these are useful for bias method during sampling
-
-			eos = tf.sigmoid(-1*eos_hat) # technically we gained a negative sign
-			pi = tf.nn.softmax(pi_hat) # softmax z_pi:
-			mu1 = mu1_hat; mu2 = mu2_hat # leave mu1, mu2 as they are
-			sigma1 = tf.exp(sigma1_hat); sigma2 = tf.exp(sigma2_hat) # exp for sigmas
-			rho = tf.tanh(rho_hat) # tanh for rho (squish between -1 and 1)rrr
-
-			return [eos, pi, mu1, mu2, sigma1, sigma2, rho]
 	# ----- for restoring previous models
 	def try_load_model(self, save_path):
 		load_was_success = True # yes, I'm being optimistic
