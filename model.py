4fbe6b20c5437efdc2c79129bad65b9402cdedf5
Amr Mohamed Nasr <re13amr@gmail.com>
2017-08-15 13:36:39 +0200

    modifications to model : remove device specification

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ class Model():
 		server = args.server
 
 
-		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d/gpu:0" % self.task_index,cluster=cluster)):
+		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d" % self.task_index,cluster=cluster)):
 			# ----- build the basic recurrent network architecture
 			# Creates 3 cells, each containing rnn_size (Default = 100) hidden unit (Basically 100 neuron OR sigmoid)
 
@@ -81,56 +81,6 @@ class Model():
 			# Output of cell 0 and next state (final state) of cell 0
 			outs_cell0, self.fstate_cell0 = tf.contrib.legacy_seq2seq.rnn_decoder(inputs, self.istate_cell0, self.cell0, loop_function=None, scope='cell0')
 
-	# ----- build the gaussian character window
-		def get_window(alpha, beta, kappa, c):
-			# phi -> [? x 1 x ascii_steps] and is a tf matrix
-			# c -> [? x ascii_steps x alphabet] and is a tf matrix
-			ascii_steps = c.get_shape()[1].value #number of items in sequence
-			phi = get_phi(ascii_steps, alpha, beta, kappa)
-			# Window shape ==> 32,1,54
-			window = tf.matmul(phi,c)
-			# Window shape ==> 32,54
-			window = tf.squeeze(window, [1]) # window ~ [?,alphabet]
-			return window, phi
-
-		#get phi for all t,u (returns a [1 x tsteps] matrix) that defines the window
-		def get_phi(ascii_steps, alpha, beta, kappa):
-			# alpha, beta, kappa -> [?,kmixtures,1] and each is a tf variable
-			# np.linspace ==> Creates a float array starting from 0 till ascii_steps -1
-			u = np.linspace(0,ascii_steps-1,ascii_steps) # weight all the U items in the sequence
-			# Kappa_term will have 32 columns, 1 row, each row contains 6
-			# Subtraction and multiplication supports broadcasting CHECK : https://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
-			kappa_term = tf.square( tf.subtract(kappa,u))
-			# exp_term shape ==> 32,1,6
-			# beta shape ==> 32,1,1
-			exp_term = tf.multiply(-beta,kappa_term)
-			# alpha shape ==> 32,1,1
-			# phi_k shape ==> 32,1,6
-			phi_k = tf.multiply(alpha, tf.exp(exp_term))
-			# phi shape ==> 32,1,6
-			# reduce sum keeps the dimensions but sums up the elements in each dimension
-			# Why?
-			phi = tf.reduce_sum(phi_k,1, keep_dims=True)
-			return phi # phi ~ [?,1,ascii_steps]
-
-		def get_window_params(i, out_cell0, kmixtures, prev_kappa, reuse=True):
-			hidden = out_cell0.get_shape()[1]
-			n_out = 3*kmixtures
-			with tf.variable_scope('window',reuse=reuse):
-				window_w = tf.get_variable("window_w", [hidden, n_out], initializer=self.graves_initializer)
-				window_b = tf.get_variable("window_b", [n_out], initializer=self.window_b_initializer)
-
-			# Matrix multiplication of X (2D tensor in this case, the output of cell 0), with the Weight, and adds the Bias
-			# w.x + b 
-			abk_hats = tf.nn.xw_plus_b(out_cell0, window_w, window_b) # abk_hats ~ [?,n_out]
-			# e^ tf reshaped
-			# Why?
-			abk = tf.exp(tf.reshape(abk_hats, [-1, 3*kmixtures,1])) # abk_hats ~ [?,n_out] = "alpha, beta, kappa hats"
-
-			alpha, beta, kappa = tf.split(abk, 3, 1) # alpha_hat, etc ~ [?,kmixtures]
-			kappa = kappa + prev_kappa
-			return alpha, beta, kappa # each ~ [?,kmixtures,1]
-		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d/gpu:0" % self.task_index,cluster=cluster)):
 
 			# Default KMixture = 1
 			# Ascii_steps = tsteps / tsteps_per_ascii (Number of ascii chars (Default 150/25 = 6))
@@ -184,8 +134,105 @@ class Model():
 			# Output shape ==> 4800, 49
 			output = tf.nn.xw_plus_b(out_cell2, mdn_w, mdn_b) #data flows through dense nn
 
+			# reshape target data (as we did the input data)
+			# flat_target_data shape ==> ?,3  
+			# (Most probably tsteps * number of data )
 
-	# ----- build mixture density cap on top of second recurrent cell
+			flat_target_data = tf.reshape(self.target_data,[-1, 3])
+			# Each array will contain ?,1
+			[x1_data, x2_data, eos_data] = tf.split(flat_target_data, 3, 1) #we might as well split these now
+			# two mu & two sigma because we are using bernolli to detect eos & bivariante to predict next point.
+			[self.eos, self.pi, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho] = get_mdn_coef(output)
+
+			loss = get_loss(self.pi, x1_data, x2_data, eos_data, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho, self.eos)
+			self.cost = loss / (self.batch_size * self.tsteps)
+
+
+			# ----- bring together all variables and prepare for training
+			
+			self.learning_rate = tf.Variable(0.0, trainable=False)
+			self.decay = tf.Variable(0.0, trainable=False)
+			self.momentum = tf.Variable(0.0, trainable=False)
+			tvars = tf.trainable_variables()
+
+			logger.write("First half gradient")
+			testGradient2 = tf.gradients(self.cost, tvars[len(tvars)/2:])
+			logger.write("Second half gradient")
+			testGradient1 = tf.gradients(self.cost, tvars[:len(tvars)/2])
+
+			testGradient = testGradient1+testGradient2
+
+			grads, _ = tf.clip_by_global_norm(testGradient, self.grad_clip)
+			if args.optimizer == 'adam':
+				self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+			elif args.optimizer == 'rmsprop':
+				self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate, decay=self.decay, momentum=self.momentum)
+			else:
+				raise ValueError("Optimizer type not recognized")
+			self.train_op = self.optimizer.apply_gradients(zip(grads, tvars))
+
+		# ----- some TensorFlow I/O
+		# Uncomment the following line to know the device used by each operation (GPU or CPU for debugging)
+		# self.sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
+		self.assign_decay = tf.assign(self.decay, args.decay )
+		self.assign_momentum = tf.assign(self.momentum, args.momentum )
+		self.assign_learning_rate = tf.assign(self.learning_rate, args.learning_rate)
+		self.saver = tf.train.Saver(tf.global_variables())
+		sv = tf.train.Supervisor(is_chief=(self.task_index == 0), init_op=tf.global_variables_initializer())
+		config = tf.ConfigProto(allow_soft_placement = True)
+		self.sess = sv.prepare_or_wait_for_session(server.target,config=config)
+
+		# ----- build the gaussian character window
+		def get_window(alpha, beta, kappa, c):
+			# phi -> [? x 1 x ascii_steps] and is a tf matrix
+			# c -> [? x ascii_steps x alphabet] and is a tf matrix
+			ascii_steps = c.get_shape()[1].value #number of items in sequence
+			phi = get_phi(ascii_steps, alpha, beta, kappa)
+			# Window shape ==> 32,1,54
+			window = tf.matmul(phi,c)
+			# Window shape ==> 32,54
+			window = tf.squeeze(window, [1]) # window ~ [?,alphabet]
+			return window, phi
+
+		#get phi for all t,u (returns a [1 x tsteps] matrix) that defines the window
+		def get_phi(ascii_steps, alpha, beta, kappa):
+			# alpha, beta, kappa -> [?,kmixtures,1] and each is a tf variable
+			# np.linspace ==> Creates a float array starting from 0 till ascii_steps -1
+			u = np.linspace(0,ascii_steps-1,ascii_steps) # weight all the U items in the sequence
+			# Kappa_term will have 32 columns, 1 row, each row contains 6
+			# Subtraction and multiplication supports broadcasting CHECK : https://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
+			kappa_term = tf.square( tf.subtract(kappa,u))
+			# exp_term shape ==> 32,1,6
+			# beta shape ==> 32,1,1
+			exp_term = tf.multiply(-beta,kappa_term)
+			# alpha shape ==> 32,1,1
+			# phi_k shape ==> 32,1,6
+			phi_k = tf.multiply(alpha, tf.exp(exp_term))
+			# phi shape ==> 32,1,6
+			# reduce sum keeps the dimensions but sums up the elements in each dimension
+			# Why?
+			phi = tf.reduce_sum(phi_k,1, keep_dims=True)
+			return phi # phi ~ [?,1,ascii_steps]
+
+		def get_window_params(i, out_cell0, kmixtures, prev_kappa, reuse=True):
+			hidden = out_cell0.get_shape()[1]
+			n_out = 3*kmixtures
+			with tf.variable_scope('window',reuse=reuse):
+				window_w = tf.get_variable("window_w", [hidden, n_out], initializer=self.graves_initializer)
+				window_b = tf.get_variable("window_b", [n_out], initializer=self.window_b_initializer)
+
+			# Matrix multiplication of X (2D tensor in this case, the output of cell 0), with the Weight, and adds the Bias
+			# w.x + b 
+			abk_hats = tf.nn.xw_plus_b(out_cell0, window_w, window_b) # abk_hats ~ [?,n_out]
+			# e^ tf reshaped
+			# Why?
+			abk = tf.exp(tf.reshape(abk_hats, [-1, 3*kmixtures,1])) # abk_hats ~ [?,n_out] = "alpha, beta, kappa hats"
+
+			alpha, beta, kappa = tf.split(abk, 3, 1) # alpha_hat, etc ~ [?,kmixtures]
+			kappa = kappa + prev_kappa
+			return alpha, beta, kappa # each ~ [?,kmixtures,1]
+			
+			# ----- build mixture density cap on top of second recurrent cell
 		def gaussian2d(x1, x2, mu1, mu2, s1, s2, rho):
 			# define gaussian mdn (eq 24, 25 from http://arxiv.org/abs/1308.0850)
 			x_mu1 = tf.subtract(x1, mu1)
@@ -226,58 +273,6 @@ class Model():
 			rho = tf.tanh(rho_hat) # tanh for rho (squish between -1 and 1)rrr
 
 			return [eos, pi, mu1, mu2, sigma1, sigma2, rho]
-
-		# reshape target data (as we did the input data)
-		# flat_target_data shape ==> ?,3  
-		# (Most probably tsteps * number of data )
-		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d/gpu:0" % self.task_index,cluster=cluster)):
-
-			flat_target_data = tf.reshape(self.target_data,[-1, 3])
-			# Each array will contain ?,1
-			[x1_data, x2_data, eos_data] = tf.split(flat_target_data, 3, 1) #we might as well split these now
-			# two mu & two sigma because we are using bernolli to detect eos & bivariante to predict next point.
-			[self.eos, self.pi, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho] = get_mdn_coef(output)
-
-			loss = get_loss(self.pi, x1_data, x2_data, eos_data, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho, self.eos)
-			self.cost = loss / (self.batch_size * self.tsteps)
-
-
-			# ----- bring together all variables and prepare for training
-			
-			self.learning_rate = tf.Variable(0.0, trainable=False)
-			self.decay = tf.Variable(0.0, trainable=False)
-			self.momentum = tf.Variable(0.0, trainable=False)
-			tvars = tf.trainable_variables()
-
-		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d/cpu:0" % self.task_index,cluster=cluster)):
-			logger.write("First half gradient")
-			testGradient2 = tf.gradients(self.cost, tvars[len(tvars)/2:])
-		with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:%d/gpu:0" % self.task_index,cluster=cluster)):
-			logger.write("Second half gradient")
-			testGradient1 = tf.gradients(self.cost, tvars[:len(tvars)/2])
-
-			testGradient = testGradient1+testGradient2
-
-			grads, _ = tf.clip_by_global_norm(testGradient, self.grad_clip)
-			if args.optimizer == 'adam':
-				self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-			elif args.optimizer == 'rmsprop':
-				self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate, decay=self.decay, momentum=self.momentum)
-			else:
-				raise ValueError("Optimizer type not recognized")
-			self.train_op = self.optimizer.apply_gradients(zip(grads, tvars))
-
-		# ----- some TensorFlow I/O
-		# Uncomment the following line to know the device used by each operation (GPU or CPU for debugging)
-		# self.sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
-		self.assign_decay = tf.assign(self.decay, args.decay )
-		self.assign_momentum = tf.assign(self.momentum, args.momentum )
-		self.assign_learning_rate = tf.assign(self.learning_rate, args.learning_rate)
-		self.saver = tf.train.Saver(tf.global_variables())
-		sv = tf.train.Supervisor(is_chief=(self.task_index == 0), init_op=tf.global_variables_initializer())
-		config = tf.ConfigProto(allow_soft_placement = True)
-		self.sess = sv.prepare_or_wait_for_session(server.target,config=config)
-
 	# ----- for restoring previous models
 	def try_load_model(self, save_path):
 		load_was_success = True # yes, I'm being optimistic
